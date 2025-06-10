43bf71f9b067853f549e041fc31b2f8417dce5f6
valentinmadiot <valentin.madiot@gmail.com>
2025-06-11 00:09:37 +0700

    add bottom navbar 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+import BottomNavbar1 from "./components/bottom-navbar-1/BottomNavbar1.jsx";
 
 function App() {
-  return <Home />;
+  return (
+    // <Home />;
+    <BottomNavbar1 />
+  );
 }
 
 export default App;
---
+++ src/components/bottom-navbar-1/BottomNavbar1.jsx
@@ -0,0 +1,131 @@
+import { useEffect, useState } from "react";
+import "remixicon/fonts/remixicon.css";
+import "./bottomNavbar1.css";
+
+const BottomNavbar1 = () => {
+  const [showList, setShowList] = useState(false);
+  const [activeLink, setActiveLink] = useState("accueil");
+
+  const toggleExpand = () => {
+    setShowList(!showList);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll("section[id]");
+      const scrollDown = window.scrollY;
+
+      sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute("id");
+
+        if (
+          scrollDown > sectionTop &&
+          scrollDown <= sectionTop + sectionHeight
+        ) {
+          setActiveLink(sectionId);
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div>
+      <nav className="bottom-nav1__nav">
+        <ul className="bottom-nav1__list">
+          <li>
+            <a
+              href="#accueil"
+              className={`bottom-nav1__link ${
+                activeLink === "home" ? "bottom-nav1__active-link" : ""
+              }`}>
+              <i className="ri-home-smile-2-line"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#apropos"
+              className={`bottom-nav1__link ${
+                activeLink === "apropos" ? "bottom-nav1__active-link" : ""
+              }`}>
+              <i className="ri-user-line"></i>
+            </a>
+          </li>
+          <li>
+            <button className="bottom-nav1__expand" onClick={toggleExpand}>
+              <i
+                className={`ri-add-line bottom-nav1__expand-icon ${
+                  showList ? "bottom-nav1__rotate-icon" : ""
+                }`}></i>
+            </button>
+            <ul
+              className={`bottom-nav1__expand-list ${
+                showList ? "bottom-nav1__show-list" : ""
+              }`}>
+              <li>
+                <a href="#" className="bottom-nav1__expand-link">
+                  <i className="ri-image-2-line"></i>
+                  <span>Gallerie</span>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="bottom-nav1__expand-link">
+                  <i className="ri-archive-line"></i>
+                  <span>Dossiers</span>
+                </a>
+              </li>
+              <li>
+                <a href="#" className="bottom-nav1__expand-link">
+                  <i className="ri-bookmark-3-line"></i>
+                  <span>Save</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a
+              href="#projets"
+              className={`bottom-nav1__link ${
+                activeLink === "projets" ? "bottom-nav1__active-link" : ""
+              }`}>
+              <i className="ri-briefcase-line"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#contact"
+              className={`bottom-nav1__link ${
+                activeLink === "contact" ? "bottom-nav1__active-link" : ""
+              }`}>
+              <i className="ri-send-plane-line"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <main className="bottom-nav1__container">
+        <section className="bottom-nav1__section" id="accueil">
+          <h2>ACCUEIL</h2>
+        </section>
+
+        <section className="bottom-nav1__section" id="apropos">
+          <h2>À PROPOS</h2>
+        </section>
+
+        <section className="bottom-nav1__section" id="projets">
+          <h2>PROJETS</h2>
+        </section>
+
+        <section className="bottom-nav1__section" id="contact">
+          <h2>CONTACT</h2>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default BottomNavbar1;
