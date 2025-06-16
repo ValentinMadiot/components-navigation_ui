1a9acdc8b4a6a361789ea618d3369242255a2f54
valentinmadiot <valentin.madiot@gmail.com>
2025-06-17 02:05:21 +0700

    update

--- src/components/nav-button/NavButton.jsx
+++ src/components/nav-button/NavButton.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import "./NavButton.css";
+import "./navButton.css";
 
 const containerVariants = {
   hidden: { opacity: 0 },
